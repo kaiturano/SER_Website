40a47c25a81db2df90db16f18470636a18b4378e
KaitlynU <kaiturano@gmail.com>
2023-10-29 23:54:15 -0500

    Light mode/dark mode

--- RRRApp/static/js/script.js
+++ RRRApp/static/js/script.js
@@ -1,4 +1,4 @@
-// -------------- Fade In And Out --------------
+//------------------Fade In And Out----------------
 document.addEventListener("DOMContentLoaded", function () {
     // Function to check if an element is intersecting the viewport
     function isElementIntersecting(entry) {
@@ -33,10 +33,67 @@ document.addEventListener("DOMContentLoaded", function () {
         observer.observe(member);
     });
 });
-
-// -------------- Update Padding --------------
+//------------------------Update Padding----------------------
+// Function to update the padding of the "main" element based on the height of the "nav" element
 function updateMainPadding() {
-    // Your existing updateMainPadding function, no changes
+    const logo = document.querySelector(".logo-image")
+    const logoCon = document.querySelector(".logo")
+    const navMain = document.querySelector(".navMain")
+    const altNav = document.querySelector(".altNav")
+    const menuButton = document.querySelector(".menuButton");
+    const header = document.getElementById("mainHeader");
+    const menu = document.querySelector(".dropdown-content");
+
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    if (menu.style.display === "block") {
+        // Close the menu
+        menu.style.display = "none";
+        menuButton.style.position = "relative";
+        menuButton.style.top = "";
+        menuButton.innerHTML = "&#9776;";
+        menuButton.style.marginLeft = "0px";
+        menuButton.style.marginTop = "0px";
+        header.style.height = "";
+        logoCon.style.display = 'block';
+
+        // Enable scrolling
+        enableScrolling();
+    } 
+
+    if (viewportWidth <= 890 && viewportWidth > 745){
+        logo.style.width = '40px';
+        navMain.style.display = 'flex';
+        altNav.style.display = 'none';
+        logoCon.style.padding = '0px';
+    }
+    else if(viewportWidth <= 745 ){
+        logo.style.width = '40px';
+        logoCon.style.padding = '0px'
+        navMain.style.display = 'none';
+        altNav.style.display = 'flex';
+    }
+    else{
+        logo.style.width = '100px';
+        navMain.style.display = 'flex';
+        altNav.style.display = 'none';
+        logoCon.style.padding = '10px';
+    }
+
+    if (viewportHeight <= 735) {
+        menu.classList.add('small-screen');
+    } 
+    else {
+        menu.classList.remove('small-screen');
+        menuButton.style.marginLeft = "";
+    }
+
+    const navElement = document.getElementById("mainHeader");
+    const mainElement = document.querySelector(".pageTitle");
+    if(mainElement){
+        mainElement.style.paddingTop = `${navElement.clientHeight+80}px`;
+    }
 }
 
 // Initial update when the DOM is ready
@@ -45,14 +102,24 @@ document.addEventListener("DOMContentLoaded", updateMainPadding);
 // Update padding on window resize
 window.addEventListener("resize", updateMainPadding);
 
+// Add an event listener to the document that listens for clicks
+document.addEventListener("click", function(event) {
+    const header = document.getElementById("mainHeader");
+    const menu = document.querySelector(".dropdown-content");
+    if (!header.contains(event.target)) {
+        menu.style.display = "none";
+    }
+});
+
 // Function to toggle the display of the dropdown content
 function toggleMenu() {
     const menu = document.querySelector(".dropdown-content");
-    const menuButton = document.querySelector(".mobile-menu-button");
+    const menuCon = document.querySelector(".dropdown");
+    const menuButton = document.querySelector(".menuButton");
     const header = document.getElementById("mainHeader");
     const logo = document.querySelector(".logo");
 
-    if (menu.style.display === "block" || window.getComputedStyle(menu).display === "block") {
+    if (menu.style.display === "block") {
         // Close the menu
         menu.style.display = "none";
         menuButton.style.position = "relative";
@@ -62,6 +129,9 @@ function toggleMenu() {
         menuButton.style.marginTop = "0px";
         header.style.height = "";
         logo.style.display = 'block';
+        menuCon.style.top = "";
+        menuCon.style.left = "";
+        menuCon.style.right = "";
 
         // Enable scrolling
         enableScrolling();
@@ -75,16 +145,13 @@ function toggleMenu() {
         menuButton.style.marginTop = "-120px";
         header.style.height = "100%";
         logo.style.display = 'none';
+        menuCon.style.top = "100px";
+        menuCon.style.left = "0";
+        menuCon.style.right = "0";
 
         // Disable scrolling
         disableScrolling();
     }
-}
-
-// Attach the click event listener to the mobile menu button
-const mobileMenuButton = document.querySelector(".mobile-menu-button");
-if (mobileMenuButton) {
-    mobileMenuButton.addEventListener("click", toggleMenu);
 }
 
 // Function to disable scrolling
@@ -102,12 +169,10 @@ function enableScrolling() {
     window.scrollTo(0, scrollY);
 }
 
-// Initialize the previous scroll position
 let prevScrollPos = window.pageYOffset;
 
-window.onscroll = function () {
-    // Your existing scroll behavior, no changes
-    const currentScrollPos = window.pageYOffset;
+window.onscroll = function() {
+    const currentScrollPos = window.pageYOffset;    
     const navElement = document.getElementById("mainHeader");
 
     if (prevScrollPos > currentScrollPos) {
@@ -118,18 +183,20 @@ window.onscroll = function () {
 
     prevScrollPos = currentScrollPos;
 };
-
-// ---------------- Up Arrow ----------------
+//---------------Up Arrow-------------------------------//
 document.addEventListener("DOMContentLoaded", function () {
     const scrollToTopButton = document.getElementById("scroll-to-top");
 
+    // Add a click event listener to the up arrow icon
     scrollToTopButton.addEventListener("click", () => {
+        // Scroll to the top of the page smoothly
         window.scrollTo({
             top: 0,
             behavior: "smooth"
         });
     });
 
+    // Show/hide the up arrow icon based on the user's scroll position
     window.addEventListener("scroll", () => {
         if (window.pageYOffset > 60) {
             scrollToTopButton.style.display = "block";
@@ -139,55 +206,57 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-    // -----------------------------------------Dark Mode--------------------------
+// -----------------------------------------Dark Mode--------------------------
+window.addEventListener('DOMContentLoaded', () => {
     const icon = document.querySelector(".mode");
     const switchInput = document.getElementById('switch');
-
+    
     // Initialize color mode based on local storage or system preferences
     const preferredColorMode = localStorage.getItem('colorMode');
-    if (preferredColorMode === 'light-mode') {
-        applyColorPalette('light-mode');
+    if (preferredColorMode === 'dark-mode') {
+        applyColorPalette('dark-mode');
         switchInput.checked = true;
     } else {
-        applyColorPalette('dark-mode');
+        applyColorPalette('light-mode');
         switchInput.checked = false;
     }
-
-    icon.addEventListener("click", toggleColorPalette);
-
-    function toggleColorPalette() {
-        const switchInput = document.getElementById('switch');
-        if (switchInput.checked) {
-            applyColorPalette('light-mode');
-            localStorage.setItem('colorMode', 'light-mode');
-        } else {
-            applyColorPalette('dark-mode');
-            localStorage.setItem('colorMode', 'dark-mode');
-        }
-    }
-
-    function applyColorPalette(palette) {
-        const root = document.documentElement;
-        root.className = palette;
-    }
-
-    //----------------------------Transparent Navbar--------------------------
-    window.addEventListener('scroll', function () {
-        const scrollY = window.scrollY; // See where the scroll is at
-        // ------------ transparent top stuff------------
-        const navElement = document.querySelector('header.team'); // Check if scroll position is at the top of the page (scrollY is zero)
-        if(navElement){
-            if (scrollY === 0 && document.body.style.top >= 0) {
-                // Scroll is at the top, make the header clear and remove box shadow
-                navElement.style.backgroundColor = "transparent";
-                navElement.style.boxShadow = "none";
-            } else {
-                // Scroll is not at the top, set background color and box shadow as needed
-                navElement.style.backgroundColor = "var(--background-color-header)";
-                navElement.style.boxShadow = "var(--shadow)";
-            }
-        }
+    
+    icon.addEventListener("click", () => {
+        toggleColorPalette();
     });
+});
 
-    //---------------------------Team img resize---------------
+function toggleColorPalette() {
+    const switchInput = document.getElementById('switch');
+    if (switchInput.checked) {
+        applyColorPalette('dark-mode');
+        localStorage.setItem('colorMode', 'dark-mode');
+    } else {
+        applyColorPalette('light-mode');
+        localStorage.setItem('colorMode', 'light-mode');
+    }
+}
+
+function applyColorPalette(palette) {
+    const root = document.documentElement;
+    root.className = palette;
+}
+
+//----------------------------Transparent Navbar--------------------------
+window.addEventListener('scroll', function () {
+    const scrollY = window.scrollY; // See where the scroll is at
+    // ------------ transparent top stuff------------
+    const navElement = document.querySelector('header.team'); // Check if scroll position is at the top of the page (scrollY is zero)
+    if(navElement){
+        if (scrollY === 0 && document.body.style.top >= 0) {
+            // Scroll is at the top, make the header clear and remove box shadow
+            navElement.style.backgroundColor = "transparent";
+            navElement.style.boxShadow = "none";
+        } else {
+            // Scroll is not at the top, set background color and box shadow as needed
+            navElement.style.backgroundColor = "var(--background-color-header)";
+            navElement.style.boxShadow = "var(--shadow)";
+        }
+    }
+});
 
